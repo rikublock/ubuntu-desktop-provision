fb1fbbb35ea7d5515260166f7c87428ff2e82935
Riku <riku.block@gmail.com>
2025-08-14 11:51:01 +0200

    fix: lvm vol group label

--- packages/subiquity_client/lib/src/extensions.dart
+++ packages/subiquity_client/lib/src/extensions.dart
@@ -3,8 +3,13 @@ import 'package:path/path.dart' as p;
 import 'package:subiquity_client/src/types.dart';
 
 extension DiskX on Disk {
-  /// "/dev/sda" => "sda"
-  String get sysname => p.basename(path ?? '');
+  String get sysname {
+    if (path != null) {
+      /// "/dev/sda" => "sda"
+      return p.basename(path!);
+    }
+    return label.length > 64 ? label.substring(0, 64) : label;
+  }
 }
 
 extension PartitionX on Partition {
